f19801d02b064241ee0834bc5c93a2712f55a670
yong <l1218yj2@naver.com>
2016-03-23 10:40:50 +0900

    xml..

--- Okhttp3/app/src/main/java/com/drakeash/okhttp3/MainActivity.java
+++ Okhttp3/app/src/main/java/com/drakeash/okhttp3/MainActivity.java
@@ -1,14 +1,34 @@
 package com.drakeash.okhttp3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.novoda.sexp.finder.ElementFinder;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Request;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.recvCat)
+    RecyclerView recyclerView;
+    CatAdapter catAdapter;
+    private static ElementFinder<Images> elementFinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        ButterKnife.bind(this);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        Request request = new Request.Builder()
+                .url("http://thecatapi.com/api/Images/get?format=xml&results_per_page=20")
+                .build();
+        catAdapter = new CatAdapter(MainActivity.this, null);
+        recyclerView.setAdapter(catAdapter);
+
     }
 }
